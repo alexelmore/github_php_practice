4b91e6a27210b691a12cfbdb0521af3ffe4ad4c9
Alex Elmore <alexelmore49@gmail.com>
2019-07-06 12:16:35 -0500

    Cleaned up MVC and made it so others can use it as a framework

--- app/config/config.php
+++ app/config/config.php
@@ -2,15 +2,15 @@
 
 // DB Params
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'Emily1982!');
-define('DB_NAME', 'tmvc');
+define('DB_USER', '_YOUR_USER_');
+define('DB_PASS', '_YOUR_PASS_');
+define('DB_NAME', '_YOUR_DBNAME_');
 
 //App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 
 //URL Root
-define('URLROOT', 'http://localhost/elmoremvc');
+define('URLROOT', '_YOUR_URL_');
 
 //Site Name
-define('SITENAME', 'ElmoreMvc');
+define('SITENAME', '_YOUR_SITENAME_');
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,16 +1,9 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <h1>
     <?php echo $data['title']; ?>
-</h1>;
-
-<ul>
-
-    <?php foreach ($data['posts'] as $post) : ?>
-
-        <li> <?php echo $post->title; ?></li>
-
-    <?php endforeach; ?>
-
-</ul>
+</h1>
+<p>
+    This is the ElmoreMVC PHP framework. Please refer to the docs on how to use it.
+</p>
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
